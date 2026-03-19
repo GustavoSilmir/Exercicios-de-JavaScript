cabf3fb50da36383b5bd5a4d53b2c7675adcc9c1
gustavosilmirdev <gustavosilmir@hotmail.com>
2026-03-19 19:02:27 -0300

    exemplos criados para estudos

---
+++ aula13/ambiente.js
@@ -0,0 +1,18 @@
+var  c =  1
+do { 
+    console.log(`Passo  ${c}`)
+    c++
+} while(c <=2)
+
+/*var c = 1
+while(c <= 2){
+    console.log('Tudo bem?')
+    c++
+} */
+
+/*
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+console.log('Tudo Bem?')
+*/
